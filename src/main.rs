29baf37be06ed120d59ea90d758d4995463ca118
prateushsharma <prateushsharma@gmail.com>
2025-01-19 14:14:53 +0530

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ struct Block {
     hash: String,
     ver: u32,
     prev_block: String,
-    mrkl_root: String,
+    #[serde(rename = "mrkl_root")]
+    merkle_root: String,
     time: u64,
     bits: u32,
     nonce: u32,
@@ -20,29 +21,66 @@ struct Block {
     block_index: u64,
     main_chain: bool,
     height: u32,
+    #[serde(default)]  // Make received_time optional
     received_time: u64,
-    #[serde(deserialize_with = "deserialize_string_or_struct")]
+    #[serde(default)]  // Make relayed_by optional
     relayed_by: String,
-    tx: Vec<Value>,
-}
-
-fn deserialize_string_or_struct<'de, D>(deserializer: D) -> std::result::Result<String, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let value = Value::deserialize(deserializer)?;
-    match value {
-        Value::String(s) => Ok(s),
-        _ => Ok("".to_string()),
-    }
+    tx: Vec<Transaction>,
 }
 
 #[derive(Deserialize, Debug)]
 struct Transaction {
-    from: String,
-    to: String,
-    value: String,
-    gas: String,
+    #[serde(default)]
+    hash: String,
+    #[serde(default)]
+    ver: u32,
+    #[serde(default)]
+    inputs: Vec<Input>,
+    #[serde(default)]
+    out: Vec<Output>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    sequence: u64,
+    #[serde(default)]
+    witness: String,
+    script: String,
+    index: u32,
+    prev_out: Option<PrevOut>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Output {
+    #[serde(rename = "type")]
+    output_type: u32,
+    spent: bool,
+    value: u64,
+    #[serde(default)]
+    spending_outpoints: Vec<SpendingOutpoint>,
+    n: u32,
+    tx_index: u64,
+    script: String,
+    addr: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PrevOut {
+    #[serde(rename = "type")]
+    output_type: u32,
+    spent: bool,
+    value: u64,
+    spending_outpoints: Vec<SpendingOutpoint>,
+    n: u32,
+    tx_index: u64,
+    script: String,
+    addr: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct SpendingOutpoint {
+    tx_index: u64,
+    n: u32,
 }
 
 fn main() -> Result<()> {
@@ -56,10 +94,12 @@ fn main() -> Result<()> {
         "blockhash" => {
             match fetch_block(&args[2]) {
                 Ok(block) => {
+                    println!("\nBlock Information:");
+                    println!("------------------");
                     println!("Block Hash: {}", block.hash);
                     println!("Version: {}", block.ver);
                     println!("Previous Block: {}", block.prev_block);
-                    println!("Merkle Root: {}", block.mrkl_root);
+                    println!("Merkle Root: {}", block.merkle_root);
                     println!("Timestamp: {}", block.time);
                     println!("Bits: {}", block.bits);
                     println!("Nonce: {}", block.nonce);
@@ -68,25 +108,28 @@ fn main() -> Result<()> {
                     println!("Block Index: {}", block.block_index);
                     println!("Is Main Chain: {}", block.main_chain);
                     println!("Block Height: {}", block.height);
-                    println!("Received Time: {}", block.received_time);
-                    println!("Relayed By: {}", block.relayed_by);
-                    println!("Number of transactions: {}", block.tx.len());
+                    
+                    println!("\nTransactions Summary:");
+                    println!("--------------------");
+                    println!("Total Transactions: {}", block.tx.len());
+                    
+                    // Print first few transactions as example
+                    for (i, tx) in block.tx.iter().take(5).enumerate() {
+                        println!("\nTransaction #{}", i + 1);
+                        println!("Hash: {}", tx.hash);
+                        println!("Version: {}", tx.ver);
+                        println!("Input Count: {}", tx.inputs.len());
+                        println!("Output Count: {}", tx.out.len());
+                    }
+                    
+                    if block.tx.len() > 5 {
+                        println!("\n... and {} more transactions", block.tx.len() - 5);
+                    }
                 }
                 Err(e) => println!("Failed to fetch block details: {}", e),
             }
         }
-        "transaction" => {
-            match fetch_transaction(&args[2]) {
-                Ok(transaction) => {
-                    println!("From: {}", transaction.from);
-                    println!("To: {}", transaction.to);
-                    println!("Value: {}", transaction.value);
-                    println!("Gas: {}", transaction.gas);
-                }
-                Err(e) => println!("Failed to fetch transaction details: {}", e),
-            }
-        }
-        _ => println!("Invalid command. Use 'blockhash' or 'transaction'."),
+        _ => println!("Invalid command. Use 'blockhash'."),
     }
 
     Ok(())
@@ -102,27 +145,8 @@ fn fetch_block(block_hash: &str) -> Result<Block> {
         .send()?;
 
     if response.status().is_success() {
-        let text = response.text()?;
-        println!("Raw response: {}", text);
-        
-        let block: Block = serde_json::from_str(&text)?;
+        let block = response.json::<Block>()?;
         Ok(block)
-    } else {
-        Err(Box::new(response.error_for_status().unwrap_err()))
-    }
-}
-
-fn fetch_transaction(id: &str) -> Result<Transaction> {
-    let url = format!("https://api.blockchain.com/transaction/{}", id);
-    let client = Client::new();
-
-    let response = client.get(&url)
-        .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-        .header(ACCEPT, "application/json")
-        .send()?;
-
-    if response.status().is_success() {
-        Ok(response.json()?)
     } else {
         Err(Box::new(response.error_for_status().unwrap_err()))
     }
